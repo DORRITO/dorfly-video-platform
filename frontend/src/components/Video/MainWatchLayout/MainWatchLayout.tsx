c9ee5c208fa2c12841d91a81cccf4734b0afcca7
dorrito <lickc556@gmail.com>
2026-03-01 22:27:57 +0500

    update MainWatchLayout

--- frontend/src/components/Video/MainWatchLayout/MainWatchLayout.tsx
+++ frontend/src/components/Video/MainWatchLayout/MainWatchLayout.tsx
@@ -1,6 +1,7 @@
 import s from './mainwatchlayout.module.scss'
 import VideoPlayer from './VideoPlayer'
 import VideoMeta from './VideoMeta'
+import CommentSection from './CommentSection/CommentSection'
 
 interface VideoProps {
   video: {
@@ -27,31 +28,10 @@ function MainWatchLayout({ video, comments }: VideoProps) {
             video={video}
         />
 
-        <div className={s.MainWatchLayout__comments}>
-            <div className={s.MainWatchLayout__title}>
-                Комментарии ({video._count?.comments ?? 0}):
-            </div>
-
-            {comments.map((item, index) => (
-                <div className={s.MainWatchLayout__comment}>
-                    <div className={s.MainWatchLayout__headerComment}>
-                        <div className={s.MainWatchLayout__headerCommentLeft}>
-                            <div className={s.MainWatchLayout__headerCommentAvatar}>
-                                <img src="https://static.vecteezy.com/system/resources/thumbnails/009/292/244/small/default-avatar-icon-of-social-media-user-vector.jpg" alt="" />
-                            </div>
-
-                            <div className={s.MainWatchLayout__headerCommentNickname}>
-                                {item.author.nickname}
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className={s.MainWatchLayout__commentText}>
-                        {item.text}
-                    </div>
-                </div>
-            ))}
-        </div>
+        <CommentSection 
+            video={video}
+            comments={comments}
+        />
     </div>
   )
 }
